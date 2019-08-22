8676df37ab8abf27b07c3b6423e6be9d07666070
GreywindGit <GreywindGit@users.noreply.github.com>
2019-08-22 11:27:00 +0200

    Conditional component and content display

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="selectedComponent = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent = 'appRed'">Load Red Template</button>
+                <hr>
+                <component :is="selectedComponent">
+                    <h3 slot="title">{{ title }}</h3>
+                    <p slot="artist">by {{ artist }}</p>
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data: function() {
+            return {
+                selectedComponent: 'appBlue',
+                componentDetails: {
+                    appBlue: {
+                        title: 'Blue Suede Shoes',
+                        artist: 'Elvis Presley'
+                    },
+                    appGreen: {
+                        title: 'O Green World',
+                        artist: 'Gorillaz'
+                    },
+                    appRed: {
+                        title: 'Wrapped in Red',
+                        artist: 'Kelly Clarkson'
+                    }
+                }
+            }
+        },
+        computed: {
+                title: function() { return this.componentDetails[this.selectedComponent].title },
+                artist: function() { return this.componentDetails[this.selectedComponent].artist }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/Green.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <slot name="title"></slot>
+        <slot name="artist"></slot>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        color: darkgreen;
+        border: 1px solid green;
+        background-color: lightgreen;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+</style>
